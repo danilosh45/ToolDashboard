45d898caaf4b43642f6011e06636b2e2720f3c71
Danilo Altamirano <danilosh45@gmail.com>
2019-03-28 02:14:44 +0100

    scrape finish maybe focus funtionality someone fix

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,19 @@
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from news.views import scrape
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('notes/', include('notepad.urls', namespace='notes')),
+    path('scrape', scrape, name='scrape')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- news/views.py
+++ news/views.py
@@ -1,28 +1,57 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, redirect
+import math
 import requests
-
 requests.packages.urllib3.disable_warnings()
 
 from bs4 import BeautifulSoup
+from datetime import timedelta, timezone, datetime
+import os
+import shutil
 
-def scrape():
+from .models import Headline, UserProfile
+
+
+def scrape(request):
+	user_p = UserProfile.objects.filter(user=request.user).first()
+	user_p.last_scrape = datetime.now(timezone.utc)
+	user_p.save()
+
 	session = requests.Session()
-	session.headers = {"User Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"}
+	session.headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"}
+	
 	url = 'https://www.theonion.com/'
 
 	content = session.get(url, verify=False).content
 
 	soup = BeautifulSoup(content, "html.parser")
-	posts = soup.find_all('div',{'class':'curation-module__item'}) # returns a list
-	print(len(posts))
+
+	posts = soup.find_all('div',{'class':'curation-module__item__wrapper'}) # returns a list
 
 	for i in posts:
-		link = i.find_all('a',{'class': 'js_curation-click'})[1]
+		link = i.find_all('a',{'class':'js_curation-click'})[1]['href']
+		title = i.find_all('a',{'class':'js_curation-click'})[1].text
 		image_source = i.find('img',{'class':'featured-image'})['data-src']
-		print(link.text)
-		print(image_source)
-scrape()	
 
+		# stackoverflow solution
+
+		media_root = '/Users/danilosh45/Projects/dashboard/media_root'
+		if not image_source.startswith(("data:image", "javascript")):
+			local_filename = image_source.split('/')[-1].split("?")[0]
+			r = session.get(image_source, stream=True, verify=False)
+			with open(local_filename, 'wb') as f:
+				for chunk in r.iter_content(chunk_size=1024):
+					f.write(chunk)
+
+			current_image_absolute_path = os.path.abspath(local_filename)
+			shutil.move(current_image_absolute_path, media_root)
+
+
+		# end of stackoverflow
+
+		new_headline = Headline()
+		new_headline.title = title
+		new_headline.url = link
+		new_headline.image = local_filename
+		new_headline.save()
+
+	return redirect('/home/')
